c1fb4fca5af5c7c2995f27ad5c40f95c0e25f024
Razvan373 <72612618+Razvan373@users.noreply.github.com>
2024-12-10 03:16:51 +0200

    pagina gata

--- Scripts/script.js
+++ Scripts/script.js
@@ -18,6 +18,13 @@ async function getSongs(){
     return songs
 }
 
+const playMusic = (track) =>{
+    currentSong.src = "/Cantece/" + track
+    currentSong.play()
+    play.src = "pause.svg"
+    document.querySelector(".songinfo").innerHTML = track
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00"
+}
 async function main()
 {
     let songs = await getSongs()
@@ -34,6 +41,38 @@ async function main()
     audio.addEventListener('loadeddata', () => {
         console.log(audio.duration, audio.currentSrc, audio.currentTime);
     })
+
+    /*
+    plsy.addEventListener("click",() =>{
+        if(currentSong.paused){
+            currentSong.play()
+            play.src="img\\pause.svg"
+        }
+        else[
+            currentSong.pause()
+            play.src="img\\play.svg"
+        ]
+    })
+    */
+
+    currentSong.addEventListener("timeupdate", () => {
+        console.log(currentSong.currentTime, currentSong.duration);
+    })
+
+    document.querySelector(".seekbar").addEventListener("click", e => {
+        let percet = (e.offseX/e.target.getBoundingClientRect().width)*100;
+        document.querySelector(".circle").style.left = percet + "%";
+        currentSong.currentTime = ((currentSong.duration)*percet)/100
+    })
+    document.querySelector(".hamburger").addEventListener("click", () => {
+        document.querySelector(".hamburger").style.left = "0"
+    })
+
+    document.querySelector(".close").addEventListener("click", () => {
+        document.querySelector(".hamburger").style.left = "-120%"
+    })
 }
+
+
 
 main()
